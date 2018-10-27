c942e7b1a8af63b7701eb655997c5d8f289fc30e
Presenter <hendrik.wermser@ironhack.com>
2018-10-27 19:15:34 +0200

    css html and js starter code

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,38 @@
-function deleteItem(e){
 
-}
+// I deleted all the existing functions. 
+// At some point you might want to put functionality into functions for readability :-)
+window.onload = function () {
 
-function getPriceByProduct(itemNode){
+  var calculatePriceButtonId = 'calculate-button'
+  var calculatePriceButton = document.getElementById(calculatePriceButtonId);
 
-}
+  calculatePriceButton.onclick = function () {
+    // this currently works only for the very first element, if you create more it will not calculate the prices for those.
+    // change that ! ;-)
+    var shopItemElement = document.getElementsByClassName('shop-item')[0]
+    var price = shopItemElement.getElementsByClassName('unit-price')[0].innerText
+    console.log("price : " + price) // remember to use console.log everywhere if you have trouble understanding what your code does
+    var qty = shopItemElement.getElementsByTagName('input')[0].value
+    console.log("qty : " + qty)
+    var totalPrice = Number(price) * Number(qty)
 
-function updatePriceByProduct(productPrice, index){
+    console.log("totalPrice : " + totalPrice) 
 
-}
+    shopItemElement.getElementsByClassName('total-price')[0].innerText = totalPrice.toFixed(2)
+  }
 
-function getTotalPrice() {
+  var deleteButtonId = 'delete-button'
+  var deleteButtons = document.getElementsByClassName(deleteButtonId);
 
-}
+  // this will only fetch the very first delete button. make it work for all of them once you added more elements.
+  deleteButtons[0].onclick = function () {
+    // look at https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+    // NOTE that closest() expexts a CSS-selector EXPRESSION ( thus the '.' in '.shop-item')
+    // I found this via searching google for "javascript get parent element by class"
+    deleteButtons[0].closest('.shop-item').remove() 
+  }
 
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+  for (var i = 0; i < deleteButtons.length; i++) {
+    // TODO :-)
   }
 };
