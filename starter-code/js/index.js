99b78c75d1b584b86ba022d9e5bf31139f006373
Presenter <hendrik.wermser@ironhack.com>
2018-10-27 19:29:20 +0200

    updates price-update-function to be separate function

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,30 +1,31 @@
 
+function calculatePriceButtonClick() {
+  // this currently works only for the very first element, if you create more it will not calculate the prices for those.
+  // change that ! ;-)
+  var shopItemElement = document.getElementsByClassName('shop-item')[0]
+  var price = shopItemElement.getElementsByClassName('unit-price')[0].innerText
+  console.log("price : " + price) // remember to use console.log everywhere if you have trouble understanding what your code does
+  var qty = shopItemElement.getElementsByTagName('input')[0].value
+  console.log("qty : " + qty)
+  var totalPrice = Number(price) * Number(qty)
 
+  console.log("totalPrice : " + totalPrice) 
 
-// I deleted all the existing functions. 
-// At some point you might want to put functionality into functions for readability :-)
+  shopItemElement.getElementsByClassName('total-price')[0].innerText = totalPrice.toFixed(2)
+}
+
+// I deleted all the existing functions except one.
+// At some point you might want to put more and more functionality into functions in the same way for readability :-)
 window.onload = function () {
 
   var calculatePriceButtonId = 'calculate-button'
   var calculatePriceButton = document.getElementById(calculatePriceButtonId);
 
-  calculatePriceButton.onclick = function () {
-    // this currently works only for the very first element, if you create more it will not calculate the prices for those.
-    // change that ! ;-)
-    var shopItemElement = document.getElementsByClassName('shop-item')[0]
-    var price = shopItemElement.getElementsByClassName('unit-price')[0].innerText
-    console.log("price : " + price) // remember to use console.log everywhere if you have trouble understanding what your code does
-    var qty = shopItemElement.getElementsByTagName('input')[0].value
-    console.log("qty : " + qty)
-    var totalPrice = Number(price) * Number(qty)
+  // move the 
+  calculatePriceButton.onclick = calculatePriceButtonClick;
 
-    console.log("totalPrice : " + totalPrice) 
-
-    shopItemElement.getElementsByClassName('total-price')[0].innerText = totalPrice.toFixed(2)
-  }
-
-  var deleteButtonId = 'delete-button'
-  var deleteButtons = document.getElementsByClassName(deleteButtonId);
+  var deleteButtonClass = 'delete-button'
+  var deleteButtons = document.getElementsByClassName(deleteButtonClass);
 
   // this will only fetch the very first delete button. make it work for all of them once you added more elements.
   deleteButtons[0].onclick = function () {    
